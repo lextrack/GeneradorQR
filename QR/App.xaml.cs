69203475804e2d4c2acba7ac46f730cf222b1b8b
Lextrack <arielmunozg97@gmail.com>
2025-03-24 18:43:40 -0300

    Add project files.

---
+++ QR/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace QR
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
